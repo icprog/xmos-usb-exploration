3d564d0773ff764dd752bc0be06a482385a6a8e4
Ian Daniher <it.daniher@gmail.com>
2012-11-27 23:00:21 -0500

    s/XTAG2/TEST/

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 
 import usb
-import itertools 
 
-XMOS_XTAG2_VID = 0x20b1
-XMOS_XTAG2_PID = 0x0101
-XMOS_XTAG2_EP_IN = 0x81
-XMOS_XTAG2_EP_OUT = 0x01
+XMOS_TEST_VID = 0x20b1
+XMOS_TEST_PID = 0x0101
+XMOS_TEST_EP_IN = 0x81
+XMOS_TEST_EP_OUT = 0x01
 
-dev = usb.core.find(idVendor=XMOS_XTAG2_VID, idProduct=XMOS_XTAG2_PID)
+dev = usb.core.find(idVendor=XMOS_TEST_VID, idProduct=XMOS_TEST_PID)
 while True:
-	print dev.read(XMOS_XTAG2_EP_IN, 4, 0, 1000)
+	print dev.read(XMOS_TEST_EP_IN, 4, 0, 1000)
