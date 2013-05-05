a295c31d9b8f77fbb27b07b0dbabfbff3a391204
Ian Daniher <it.daniher@gmail.com>
2013-05-05 14:29:35 -0400

    abstraction for using xtag2 as squarewave generator

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
-
+from __future__ import division
 import usb
-import time
 
 XMOS_TEST_VID = 0x59e3
 XMOS_TEST_PID = 0xf000
 
 dev = usb.core.find(idVendor=XMOS_TEST_VID, idProduct=XMOS_TEST_PID)
-from pylab import *
-for div in arange(0, 2**16):
+
+def setFrequency(freq):
+	period = 1/freq
+	tensOfNanoSeconds = period/1e-08
+	div = tensOfNanoSeconds/2
+	print div
 	div = int(div)
 	low = div & 0xFFFF
 	high = (div >> 16) & 0xFFFF
-	dev.ctrl_transfer(0x40|0x80, 0x02, high, low, 0)
-	time.sleep(1)
-print map(hex, dev.ctrl_transfer(0x40|0x80, 0x01, 0, 0, 2))
+	dev.ctrl_transfer(0x40|0x80, 0x02, low, high, 0)
+
+if __name__ == "__main__":
+	setFrequency(1e6)
